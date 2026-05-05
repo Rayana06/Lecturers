620db59c6002077ab06e3c484b6c72597a5fd2d8
Rayana <khshva.77@gmail.com>
2026-05-06 00:36:44 +0300

    Add files via upload

---
+++ lecturer-card.js
@@ -0,0 +1,38 @@
+export class LecturerCardComponent {
+    constructor(parent) {
+        this.parent = parent;
+    }
+
+    getHTML(data) {
+        return `
+            <div class="card-item">
+                <div class="card-top">
+                    <img class="teacher-photo" src="${data.image}" alt="${data.name}">
+                    
+                    <div class="card-info">
+                        <h3>${data.name}</h3>
+                        <p>${data.subject}</p>
+                    </div>
+                </div>
+
+                <div class="card-buttons">
+                    <button class="open-btn">Открыть</button>
+                    <button class="model-btn">3D</button>
+                    <button class="delete-btn">Удалить</button>
+                </div>
+            </div>
+        `;
+    }
+
+    render(data, onOpen, onShow3D, onDelete) {
+        const wrapper = document.createElement("div");
+        wrapper.innerHTML = this.getHTML(data);
+
+        const element = wrapper.firstElementChild;
+        this.parent.appendChild(element);
+
+        element.querySelector(".open-btn").addEventListener("click", onOpen);
+        element.querySelector(".model-btn").addEventListener("click", onShow3D);
+        element.querySelector(".delete-btn").addEventListener("click", onDelete);
+    }
+}
